4470f5227ef197041bbde3e5ab26453eda266dfb
Peter Sandor <pepe1125@gmail.com>
2021-10-11 14:19:08 +0200

    Add files via upload

---
+++ bolt_ido.py
@@ -0,0 +1,9 @@
+x = int(input('Mennyi az idő? '))
+if 7 < x < 14:
+    print('nyitva még:', 14 - x , 'óráig...')
+else:
+    print('zárva')
+    if x>14:
+        print(24 - x + 7 , 'óra múlva lesz nyitva!')
+    else:
+        print( 7 - x , 'óra múlva lesz nyitva!')
